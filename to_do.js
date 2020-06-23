767509b3a4f72e68750e7a1077541624c077b3b5
Ania <anna.ananicz@gmail.com>
2020-06-23 16:01:53 +0200

    added features: 'add to done' button, clear button; media queries

--- to_do.js
+++ to_do.js
@@ -1,53 +1,95 @@
 const inputField = document.getElementById('inputField')
 const todoItems = document.getElementById('todoItems')
 const doneItems = document.getElementById('doneItems')
+const createItemButton = document.getElementById('createInput');
+const clearAll = document.getElementById('clearAll')
 
 let toDos = []
 let done = []
 
-function clearInput() {
-  inputField.focus();
-  inputField.value = ' '
+function init() {
+  clearInput()
 }
 
+inputField.addEventListener('keyup', function (event) {
+  createItemButton.disabled = Boolean(!event.target)
+})
+
 function addItem() {
-  const itemToAdd = {
-    id: toDos.length + 1,
-    name: inputField.value
-  }
-  toDos.push(itemToAdd)
-  clearInput()
-  displayToDo(itemToAdd)
+    const itemToAdd = {
+      id: toDos.length + 1,
+      name: inputField.value,
+    }
+    toDos.push(itemToAdd)
+    clearInput()
+    displayToDo(itemToAdd)
 }
+
+function clearInput() {
+  inputField.focus();
+  inputField.value = ''
+  createItemButton.disabled = true
+}
+
 
 function displayToDo(element) {
   const li = document.createElement('li')
-  li.innerHTML = element.name
-  todoItems.appendChild(li)
+  li.innerHTML = element.name;
+  todoItems.appendChild(li);
+
+  const doneBtn = document.createElement('button');
+  doneBtn.className = 'doneButton';
+  doneBtn.appendChild(document.createTextNode('✓'));
+  li.appendChild(doneBtn);
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.className = 'deleteButton';
+  deleteBtn.appendChild(document.createTextNode('✗'));
+  li.appendChild(deleteBtn);
+
+  doneBtn.addEventListener('click', addToDone)
+
+  deleteBtn.addEventListener('click', removeItem);
 }
 
+function removeItem(e) {
+  let li = e.target.parentElement;
+  e.target.parentElement.remove(li);
+}
 
-function addToDone() {
-  const doneItem = toDos.pop()
-  done.push({
-    id: done.length + 1,
-    name: doneItem.name
-  })
+function addToDone(e) {
+  let doneItem = e.target.parentElement.firstChild.textContent
+  // done.push({
+  //   id: done.length + 1,
+  //   name: doneItem
+  // })
   displayDone(doneItem);
+  e.target.parentElement.remove();
+  console.log(doneItem)
 }
+
+// function addToDone() {
+//   const doneItem = toDos.pop()
+//   done.push({
+//     id: done.length + 1,
+//     name: doneItem.name
+//   })
+//   displayDone(doneItem);
+// }
 
 function displayDone(element) {
   const list = document.createElement('li')
-  list.innerHTML = element.name
+  list.innerHTML = element
   doneItems.appendChild(list)
-  refreshToDos()
+  // refreshToDos()
 }
 
-function refreshToDos() {
-  todoItems.lastChild.remove()
+// function refreshToDos() {
+//   todoItems.lastChild.remove()
+// }
+
+clearAll.addEventListener('click', clear)
+function clear() {
+  window.location.reload()
 }
-
-
-
-
 
